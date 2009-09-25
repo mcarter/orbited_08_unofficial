a94c78ead22fabc157c2981d3ba0772aa67352e2
Michael Carter <cartermichael@gmail.com>
2009-09-25 20:59:13 +0000

    update csp dependancy version

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
             static_types ] or [] for (d, s, f) in os.walk(os.path.join('orbited', 'static'))
         ]) },
     zip_safe = False,
-    install_requires = ['csp', 'Twisted'],
+    install_requires = ['csp>=0.1alpha2', 'Twisted'],
     entry_points = '''    
         [console_scripts]
         orbited = orbited.start:main
